f40f2f34d5554b1d709857c661f6299d4ed4bc0f
Uygur Tepe <uygur.t10@gmail.com>
2020-03-22 13:58:10 -0400

    created Node Class

---
+++ Node.py
@@ -0,0 +1,7 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.leftConnected = None
+        self.rightConnected = None
+        self.topConnected = None
+        self.bottomConnected = None
