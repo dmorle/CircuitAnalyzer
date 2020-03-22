d63159b864d17fdbc0a35228da9b2a18f25080e2
dmorle <morled@uwindsor.ca>
2020-03-22 14:26:20 -0400

    started node and connection class implimentations

--- Node.py
+++ Node.py
@@ -1,7 +1,21 @@
+from components import *
+
+
 class Node:
-    def __init__(self, value):
-        self.value = value
-        self.leftConnected = None
-        self.rightConnected = None
-        self.topConnected = None
-        self.bottomConnected = None
+    def __init__(self, name, isground):
+        self.name = name
+        self.isground = isground
+        self.connections = list()
+
+    def add_connection(self, cmp, polarity):
+        """
+        called by a component on initialization
+        :param cmp: component instance to be connected
+        :param polarity: True => Positive, False => Negative
+        """
+        if not isinstance(cmp, component):
+            raise TypeError("invalid component argument")
+        if type(polarity) is not bool:
+            raise TypeError("invalid polarity argument")
+
+        self.connections.append((cmp, polarity))
--- components/component.py
+++ components/component.py
@@ -1,7 +1,42 @@
-class Component:
+from abc import ABC, abstractmethod
+
+
+class Component(ABC):
     """
     provides a base class for all other components to inherit from
+
+    All components will be modelled with the following voltage-current relation:
+        AV+BI=C
     """
 
-    def __init__(self):
-        pass
+    def __init__(self, name, n1, n2):
+        """
+        Creates a component
+        :param n1: negative node connection
+        :param n2: positive node connection
+        """
+
+        self.name = name
+        self.n1 = n1
+        self.n2 = n2
+
+    @abstractmethod
+    def get_A(self):
+        """
+        calculates A from known parameters
+        :return: A from equation AV+BI=C
+        """
+
+    @abstractmethod
+    def get_B(self):
+        """
+        calculates B from known parameters
+        :return: B from equation AV+BI=C
+        """
+
+    @abstractmethod
+    def get_C(self):
+        """
+        calculates C from known parameters
+        :return: C from equation AV+BI=C
+        """
