6815858d26fac4f87bc5335c73df11416bfcab78
dmorle <morled@uwindsor.ca>
2020-03-21 21:44:00 -0400

    added inital repo setup and structure

---
+++ components/component.py
@@ -0,0 +1,7 @@
+class Component:
+    """
+    provides a base class for all other components to inherit from
+    """
+
+    def __init__(self):
+        pass
---
+++ main.py
@@ -0,0 +1,12 @@
+from components import *
+from Circuit import Circuit
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def main():
+    return
+
+
+if __name__ == "__main__":
+    main()
