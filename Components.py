a96612027d5f1c0d92e294ef428edefa20e65e1f
dmorle <morled@uwindsor.ca>
2020-03-28 21:01:03 -0400

    added voltage and current sources

--- Components.py
+++ Components.py
@@ -42,7 +42,7 @@ class Capacitor(Component):
         if "frequency" not in state_dict:
             raise KeyError("frequency not provided")
 
-        return 1, -1/(2 * pi * state_dict["frequency"] * self.capacitance), 0
+        return 1, -1 / (2 * pi * state_dict["frequency"] * self.capacitance), 0
 
 
 class Inductor(Component):
@@ -68,6 +68,48 @@ class Inductor(Component):
         return 1, -2 * pi * state_dict["frequency"] * self.inductance, 0
 
 
+class VoltageSource(Component):
+    def __init__(self, name, n_neg, n_pos, voltage):
+        """
+        :type name: str
+        :type n_neg: Node
+        :type n_pos: Node
+        :type voltage: float
+        :param name: name of the component
+        :param n_neg: negative node connection
+        :param n_pos: positive node connection
+        :param voltage: voltage across the source
+        """
+        super(VoltageSource, self).__init__(name, n_neg, n_pos)
+
+        self.voltage = voltage
+
+    def get_params(self, state_dict):
+        return 1, 0, self.voltage
+
+
+class CurrentSource(Component):
+    def __init__(self, name, n_neg, n_pos, current):
+        """
+        :type name: str
+        :type n_neg: Node
+        :type n_pos: Node
+        :type current: float
+        :param name: name of the component
+        :param n_neg: negative node connection
+        :param n_pos: positive node connection
+        :param current: current through the source
+        """
+        super(CurrentSource, self).__init__(name, n_neg, n_pos)
+
+        self.current = current
+
+    def get_params(self, state_dict):
+        return 0, 1, self.current
+
+
 Resistor.get_params.__doc__ = Component.get_params.__doc__
 Capacitor.get_params.__doc__ = Component.get_params.__doc__
 Inductor.get_params.__doc__ = Component.get_params.__doc__
+VoltageSource.get_params.__doc__ = Component.get_params.__doc__
+CurrentSource.get_params.__doc__ = Component.get_params.__doc__
