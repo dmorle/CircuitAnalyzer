89994642a33516cec08cb05b80d1a9802576617e
dmorle <morled@uwindsor.ca>
2020-03-28 21:16:06 -0400

    added component unit tests

--- UnitTests.py
+++ UnitTests.py
@@ -1,5 +1,6 @@
 import unittest
-from Circuit import Circuit, Node
+from Circuit import *
+from Components import *
 
 
 class CircuitBuilding(unittest.TestCase):
@@ -21,6 +22,39 @@ class CircuitBuilding(unittest.TestCase):
             len(circ.nodes) == 2 and
             len(circ.connections) == 0
         )
+
+    def test_component_circuit_1(self):
+        circ = Circuit()
+
+        ground = Node("ground node", True)
+        node = Node("non-ground")
+
+        circ.add_node(ground)
+        circ.add_node(node)
+
+        Resistor("R", node, ground, 1000)
+        VoltageSource("V0", ground, node, 5)
+
+        self.assertTrue(
+            len(circ.nodes) == 2 and
+            len(circ.connections) == 2
+        )
+
+    def test_component_rejection(self):
+        circ = Circuit()
+
+        ground = Node("ground node", True)
+        node = Node("non-ground")
+
+        circ.add_node(ground)
+
+        try:
+            Resistor("R", node, ground, 1000)
+        except CircuitError as e:
+            self.assertEqual(str(e), "node non-ground does not belong to a circuit", "Incorrect exception type")
+            return
+
+        self.assertTrue(False, "Component was not rejected")
 
 
 if __name__ == '__main__':
